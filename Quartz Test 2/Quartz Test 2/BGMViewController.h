01946e1ca176daf14255be014ca9334578b6c4a6
Ma Yan <896098846@qq.com>
2014-06-19 12:58:18 +0900

    Project Created

---
+++ Quartz Test 2/Quartz Test 2/BGMViewController.h
@@ -0,0 +1,13 @@
+//
+//  BGMViewController.h
+//  Quartz Test 2
+//
+//  Created by 馬 岩 on 14-6-19.
+//  Copyright (c) 2014年 馬 岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGMViewController : UIViewController
+
+@end
---
+++ Quartz Test 2/Quartz Test 2Tests/Quartz_Test_2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Quartz_Test_2Tests.m
+//  Quartz Test 2Tests
+//
+//  Created by 馬 岩 on 14-6-19.
+//  Copyright (c) 2014年 馬 岩. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Quartz_Test_2Tests : XCTestCase
+
+@end
+
+@implementation Quartz_Test_2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
